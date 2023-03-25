1962f41116e8f653287aa590696aea53cdac711b
Rushabh <rushabhnagpure864@gmail.com>
2023-03-25 12:07:45 +0530

    new things is added  in it

--- src/main/java/com/driver/StudentController.java
+++ src/main/java/com/driver/StudentController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("students")
 public class StudentController {
-    @Autowired
+    @Autowiredi
     StudentService service;
 
     @PostMapping("/add-student")
